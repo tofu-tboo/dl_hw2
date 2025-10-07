68d113d05d008734d353cef1ba8976249f4b5f9d
DGIST2023choidoyun <cdydoyun@dgist.ac.kr>
2025-10-07 22:22:44 +0900

    init

---
+++ emo_utils.py
@@ -0,0 +1,60 @@
+import csv
+import numpy as np
+import emoji
+import pandas as pd
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r', encoding="utf8") as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+        
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+
+def read_csv(filename = 'emojify_data.csv'):
+    phrase = []
+    emoji = []
+
+    with open (filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+
+        for row in csvReader:
+            phrase.append(row[0])
+            emoji.append(row[1])
+
+    X = np.asarray(phrase)
+    Y = np.asarray(emoji, dtype=int)
+
+    return X, Y
+
+
+emoji_dictionary = {"0": "\u2764\uFE0F",    # :heart: prints a black instead of red heart depending on the font
+                    "1": ":baseball:",
+                    "2": ":smile:",
+                    "3": ":disappointed:",
+                    "4": ":fork_and_knife:"}
+
+def label_to_emoji(label):
+    """
+    Converts a label (int or string) into the corresponding emoji code (string) ready to be printed
+    """
+    return emoji.emojize(emoji_dictionary[str(label)], use_aliases=True)
+              
+    
+def print_predictions(X, pred):
+    print()
+    for i in range(X.shape[0]):
+        print(X[i], label_to_emoji(int(pred[i])))
+    
